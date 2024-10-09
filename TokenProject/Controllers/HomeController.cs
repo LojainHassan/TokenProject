6553244ba447e720e36495510cc600f108a37afb
lojainhassan998l <lojainhassan998@outlook.com>
2024-10-09 16:34:50 +0300

    Set up token

---
+++ TokenProject/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TokenProject.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult SayHello()
+        {
+            return Ok("Hello My Client");
+        }
+
+
+    }
+}
